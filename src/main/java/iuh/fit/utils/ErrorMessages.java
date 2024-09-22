f656c909b4263c48455b150ce9451374470cb2e0
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2024-09-22 17:03:35 +0700

    Thêm các ErrorMessages mới cho Account

--- src/main/java/iuh/fit/utils/ErrorMessages.java
+++ src/main/java/iuh/fit/utils/ErrorMessages.java
@@ -6,4 +6,7 @@ public class ErrorMessages {
     public static final String EMP_INVALID_PHONENUMBER = "Số điện thoại từ 8 đến 11 ký số";
     public static final String EMP_INVALID_ADDRESS = "Email phải từ 4 đến 30 ký tự và không cứ ký tự đặc biệt";
     public static final String EMP_INVALID_DOB = "Ngày sinh không hợp lý, tuổi ít nhât từ 18 trở lên";
+    public static final String ACC_INVALID_ID = "Mã tài khoản phải theo định dạng ACC-XXXXXX, với X là ký số";
+    public static final String ACC_INVALID_USERNAME = "Tên đăng nhập phải có ít nhất 5 ký tự và không vượt quá 20 ký tự";
+    public static final String ACC_INVALID_PASSWORD = "Mật khẩu phải từ 8 đến 30 ký tự\nCó ít nhất một chữ cái, một chữ số và một kí tự đặc biệt như !@#$%^&*()";
 }
