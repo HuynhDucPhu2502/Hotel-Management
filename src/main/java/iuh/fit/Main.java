6c626fdcb074609b0a1b7827ff27bd1a736fd4fd
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2024-09-25 11:29:19 +0700

    commented DAO fetch on main

--- src/main/java/iuh/fit/Main.java
+++ src/main/java/iuh/fit/Main.java
@@ -10,7 +10,7 @@ import iuh.fit.models.enums.AccountStatus;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        EmployeeDAO.getEmployees().forEach(System.out::println);
-        ShiftDAO.getShifts().forEach(System.out::println);
+//        EmployeeDAO.getEmployees().forEach(System.out::println);
+//        ShiftDAO.getShifts().forEach(System.out::println);
     }
 }
