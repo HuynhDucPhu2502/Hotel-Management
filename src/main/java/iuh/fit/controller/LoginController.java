582681af4590bd7d7900f92f18c972e783dffc6c
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2024-09-27 18:25:28 +0700

    add password visibility

--- src/main/java/iuh/fit/controller/LoginController.java
+++ src/main/java/iuh/fit/controller/LoginController.java
@@ -1,4 +1,36 @@
 package iuh.fit.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    private PasswordField hiddenPasswordField; // ẩn mật khẩu
+
+    @FXML
+    private TextField visiblePasswordField; // hiện mật khẩu
+
+    @FXML
+    private CheckBox passwordCheckbox; // checkbox ẩn hiện mật khẩu
+
+    @FXML
+    private void initialize() {
+        hiddenPasswordField.textProperty().bindBidirectional(visiblePasswordField.textProperty());
+
+        passwordCheckbox.setOnAction(event -> {
+            if (hiddenPasswordField.isVisible()) {
+                hiddenPasswordField.setVisible(false);
+                hiddenPasswordField.setManaged(false);
+                visiblePasswordField.setVisible(true);
+                visiblePasswordField.setManaged(true);
+            } else {
+                visiblePasswordField.setVisible(false);
+                visiblePasswordField.setManaged(false);
+                hiddenPasswordField.setVisible(true);
+                hiddenPasswordField.setManaged(true);
+            }
+        });
+    }
 }
