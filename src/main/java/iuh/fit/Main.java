c9fda5856cbc4237184635dcae91f66c26dcad3a
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2024-09-28 12:09:02 +0700

    add menu

--- src/main/java/iuh/fit/Main.java
+++ src/main/java/iuh/fit/Main.java
@@ -1,6 +1,5 @@
 package iuh.fit;
 
-import iuh.fit.dao.EmployeeDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,7 +10,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/iuh/fit/view/LoginPanel.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/iuh/fit/view/MenuBar.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Quản Lý Khách Sạn");
         stage.setScene(scene);
