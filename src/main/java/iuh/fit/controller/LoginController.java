d49855d775d24b19e2d0061dc837044615327bc0
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2024-09-28 15:51:31 +0700

    make in login able

--- src/main/java/iuh/fit/controller/LoginController.java
+++ src/main/java/iuh/fit/controller/LoginController.java
@@ -1,11 +1,22 @@
 package iuh.fit.controller;
 
+import iuh.fit.dao.AccountDAO;
+import iuh.fit.models.Account;
+import iuh.fit.utils.ErrorMessages;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class LoginController {
+    @FXML
+    private TextField userNameField;
+
     @FXML
     private PasswordField hiddenPasswordField; // ẩn mật khẩu
 
@@ -16,21 +27,63 @@ public class LoginController {
     private CheckBox passwordCheckbox; // checkbox ẩn hiện mật khẩu
 
     @FXML
+    private Text errorMessage;
+
+    @FXML
+    private Button signInButton;
+
+
+    @FXML
     private void initialize() {
         hiddenPasswordField.textProperty().bindBidirectional(visiblePasswordField.textProperty());
 
-        passwordCheckbox.setOnAction(event -> {
-            if (hiddenPasswordField.isVisible()) {
-                hiddenPasswordField.setVisible(false);
-                hiddenPasswordField.setManaged(false);
-                visiblePasswordField.setVisible(true);
-                visiblePasswordField.setManaged(true);
-            } else {
-                visiblePasswordField.setVisible(false);
-                visiblePasswordField.setManaged(false);
-                hiddenPasswordField.setVisible(true);
-                hiddenPasswordField.setManaged(true);
+        passwordCheckbox.setOnAction(event -> PasswordVisibility());
+        signInButton.setOnAction(event -> signIn());
+
+    }
+
+    private void PasswordVisibility() {
+        if (hiddenPasswordField.isVisible()) {
+            hiddenPasswordField.setVisible(false);
+            hiddenPasswordField.setManaged(false);
+            visiblePasswordField.setVisible(true);
+            visiblePasswordField.setManaged(true);
+        } else {
+            visiblePasswordField.setVisible(false);
+            visiblePasswordField.setManaged(false);
+            hiddenPasswordField.setVisible(true);
+            hiddenPasswordField.setManaged(true);
+        }
+    }
+
+    private void signIn() {
+        String userName = userNameField.getText();
+        if (userName.isEmpty()) {
+            errorMessage.setText(ErrorMessages.LOGIN_INVALID_USERNAME);
+            return;
+        }
+
+        String password = hiddenPasswordField.getText();
+        if (password.isEmpty()) {
+            errorMessage.setText(ErrorMessages.LOGIN_INVALID_PASSWORD);
+            return;
+        }
+
+        Account account = AccountDAO.getLogin(userName, password);
+
+        if (account == null) {
+            errorMessage.setText(ErrorMessages.LOGIN_INVALID_ACCOUNT);
+        } else {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/iuh/fit/view/Dashboard.fxml"));
+                Scene dashboardScene = new Scene(fxmlLoader.load());
+
+                // Lấy Stage hiện tại và set Scene mới
+                Stage currentStage = (Stage) signInButton.getScene().getWindow();
+                currentStage.setScene(dashboardScene);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        }
     }
 }
