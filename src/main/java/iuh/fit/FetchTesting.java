b7d4f09c0793418d4bbf6e3f607ea78b25e6e532
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2024-09-28 18:15:39 +0700

    update service category dao

--- src/main/java/iuh/fit/FetchTesting.java
+++ src/main/java/iuh/fit/FetchTesting.java
@@ -1,22 +1,34 @@
 package iuh.fit;
 
 import iuh.fit.dao.*;
+import iuh.fit.models.ServiceCategory;
 
 public class FetchTesting {
     public static void main(String[] args) {
-        EmployeeDAO.getEmployees().forEach(System.out::println);
-        System.out.println();
-        ShiftDAO.getShifts().forEach(System.out::println);
-        System.out.println();
-        RoomUsageServiceDAO.getRoomUsageService().forEach(System.out::println);
-        System.out.println();
-        HotelServiceDAO.getHotelService().forEach(System.out::println);
-        System.out.println();
+//        EmployeeDAO.getEmployees().forEach(System.out::println);
+//        System.out.println();
+//        ShiftDAO.getShifts().forEach(System.out::println);
+//        System.out.println();
+//        RoomUsageServiceDAO.getRoomUsageService().forEach(System.out::println);
+//        System.out.println();
+//        HotelServiceDAO.getHotelService().forEach(System.out::println);
+//        System.out.println();
+//        testServiceCategoryDAO();
+//        System.out.println();
+//        AccountDAO.getEmployees().forEach(System.out::println);
+//        System.out.println();
+//        System.out.println(AccountDAO.getLogin("huynhducphu", "test123@"));
+//        System.out.println();
+
+
+    }
+
+    private static void testServiceCategoryDAO() {
         ServiceCategoryDAO.getServiceCategory().forEach(System.out::println);
         System.out.println();
-        AccountDAO.getEmployees().forEach(System.out::println);
-        System.out.println();
-        System.out.println(AccountDAO.getLogin("huynhducphu", "test123@"));
-
+        System.out.println(ServiceCategoryDAO.getDataByID("SC-000001"));
+        ServiceCategoryDAO.createData(new ServiceCategory("SC-999999", "Test"));
+        System.out.println(ServiceCategoryDAO.getDataByID("SC-999999"));
+        ServiceCategoryDAO.deleteData("SC-999999");
     }
 }
