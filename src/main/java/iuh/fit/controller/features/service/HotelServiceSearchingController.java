f93c58f323e2f0493323435be9581fdc0b0a5f13
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2024-10-17 22:52:07 +0700

    add new panel: HotelServiceSearchingPanel

---
+++ src/main/java/iuh/fit/controller/features/service/HotelServiceSearchingController.java
@@ -0,0 +1,168 @@
+package iuh.fit.controller.features.service;
+
+import iuh.fit.dao.HotelServiceDAO;
+import iuh.fit.dao.ServiceCategoryDAO;
+import iuh.fit.models.HotelService;
+import iuh.fit.models.ServiceCategory;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class HotelServiceSearchingController {
+
+    // Table
+    // Table
+    @FXML
+    private TableView<HotelService> hotelServiceTableView;
+    @FXML
+    private TableColumn<HotelService, String> serviceIDColumn;
+    @FXML
+    private TableColumn<HotelService, String> serviceNameColumn;
+    @FXML
+    private TableColumn<HotelService, String> priceColumn;
+    @FXML
+    private TableColumn<HotelService, String> serviceCategoryColumn;
+    @FXML
+    private TableColumn<HotelService, String> descriptionColumn;
+    @FXML
+    private TableColumn<HotelService, Void> actionColumn;
+
+    private ObservableList<HotelService> items;
+
+    public void initialize() {
+        loadData();
+        setupTable();
+    }
+
+    // Phương thức load dữ liệu lên giao diện
+    private void loadData() {
+        List<HotelService> hotelServiceList = HotelServiceDAO.getHotelService();
+        items = FXCollections.observableArrayList(hotelServiceList);
+        hotelServiceTableView.setItems(items);
+        hotelServiceTableView.refresh();
+    }
+
+    // Phương thức đổ dữ liệu vào bảng
+    private void setupTable() {
+        serviceIDColumn.setCellValueFactory(new PropertyValueFactory<>("serviceId"));
+        serviceNameColumn.setCellValueFactory(new PropertyValueFactory<>("serviceName"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("servicePrice"));
+
+        serviceCategoryColumn.setCellValueFactory(data -> {
+            ServiceCategory category = data.getValue().getServiceCategory();
+            String categoryName = (category != null && category.getServiceCategoryName() != null)
+                    ? category.getServiceCategoryName()
+                    : "KHÔNG CÓ";
+            return new SimpleStringProperty(categoryName);
+        });
+
+        setupHotelServiceDescriptionColumn();
+        setupActionColumn();
+
+        hotelServiceTableView.setItems(items);
+    }
+
+    // setup cho cột mô tả
+    // THAM KHẢO
+    private void setupHotelServiceDescriptionColumn() {
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        descriptionColumn.setCellFactory(column -> new TableCell<>() {
+            private final Text text = new Text();
+
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setGraphic(null);
+                    setText(null);
+                    setStyle(null);
+                } else {
+                    text.setText(item);
+                    text.wrappingWidthProperty().bind(descriptionColumn.widthProperty());
+                    setGraphic(text);
+
+                    TableRow<?> currentRow = getTableRow();
+
+                    currentRow.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> updateTextColor(currentRow));
+
+                    hotelServiceTableView.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+                        if (currentRow.isSelected()) {
+                            updateTextColor(currentRow);
+                        }
+                    });
+
+                }
+            }
+
+            private void updateTextColor(TableRow<?> currentRow) {
+                if (currentRow.isSelected()) {
+                    if (hotelServiceTableView.isFocused()) {
+                        text.setStyle("-fx-fill: white;");
+                    } else {
+                        text.setStyle("-fx-fill: black;");
+                    }
+                } else {
+                    text.setStyle("-fx-fill: black;");
+                }
+            }
+        });
+    }
+
+    // setup cho cột thao tác
+    // THAM KHẢO
+    private void setupActionColumn() {
+        Callback<TableColumn<HotelService, Void>, TableCell<HotelService, Void>> cellFactory = new Callback<>() {
+            @Override
+            public TableCell<HotelService, Void> call(final TableColumn<HotelService, Void> param) {
+                return new TableCell<>() {
+
+                    private final Button updateButton = new Button("Cập nhật");
+                    private final Button deleteButton = new Button("Xóa");
+                    private final HBox hBox = new HBox(10);
+
+                    {
+                        updateButton.getStyleClass().add("button-update");
+                        deleteButton.getStyleClass().add("button-delete");
+
+                        hBox.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/iuh/fit/styles/Button.css")).toExternalForm());
+
+                        updateButton.setOnAction(event -> {
+                            HotelService hotelService = getTableView().getItems().get(getIndex());
+                        });
+
+                        deleteButton.setOnAction(event -> {
+                            HotelService hotelService = getTableView().getItems().get(getIndex());
+                        });
+
+                        hBox.setAlignment(Pos.CENTER);
+                        hBox.getChildren().addAll(updateButton, deleteButton);
+                    }
+
+                    @Override
+                    protected void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(hBox);
+                        }
+                    }
+                };
+            }
+        };
+
+        actionColumn.setCellFactory(cellFactory);
+    }
+
+}
