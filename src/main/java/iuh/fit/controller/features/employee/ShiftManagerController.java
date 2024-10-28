b4b8aae609fdd8213f88b8fae441f2a47695795a
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2024-10-28 15:46:17 +0700

    thêm controller mới ShiftManager

---
+++ src/main/java/iuh/fit/controller/features/employee/ShiftManagerController.java
@@ -0,0 +1,12 @@
+package iuh.fit.controller.features.employee;
+
+/**
+ * @author Le Tran Gia Huy
+ * @created 28/10/2024 - 1:59 PM
+ * @project HotelManagement
+ * @package iuh.fit.controller.features.employee
+ */
+public class ShiftManagerController {
+
+
+}
