7fbc2dfbe9e4ea9a3017af503e25d70d5cc6f8ea
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2024-10-28 23:40:05 +0700

    Thêm GlobalConstants mới

--- src/main/java/iuh/fit/utils/GlobalConstants.java
+++ src/main/java/iuh/fit/utils/GlobalConstants.java
@@ -3,6 +3,7 @@ package iuh.fit.utils;
 import java.time.LocalTime;
 
 public class GlobalConstants {
+    public static final String SHIFT_PREFIX = "SHIFT";
     public static final LocalTime SHIFT_MAX_TIME = LocalTime.of(23, 0);
     public static final LocalTime SHIFT_MIN_TIME = LocalTime.of(5, 0);
     public static final int SHIFT_MIN_WORK_HOURS = 6;
