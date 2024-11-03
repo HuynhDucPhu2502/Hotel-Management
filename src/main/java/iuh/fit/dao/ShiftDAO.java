1483976e9f56d68bb64e17587a026a07b01e2cd1
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2024-11-03 20:44:00 +0700

    Thêm một vài phương thức mới trong ShiftDAO

--- src/main/java/iuh/fit/dao/ShiftDAO.java
+++ src/main/java/iuh/fit/dao/ShiftDAO.java
@@ -1,5 +1,7 @@
 package iuh.fit.dao;
 
+import iuh.fit.models.Employee;
+import iuh.fit.models.Room;
 import iuh.fit.models.Shift;
 import iuh.fit.utils.ConvertHelper;
 import iuh.fit.utils.DBHelper;
@@ -82,6 +84,37 @@ public class ShiftDAO {
         return null;
     }
 
+    public static List<Shift> findDataByAnyContainsId(String input) {
+        List<Shift> data = new ArrayList<Shift>();
+        String sql = "SELECT shiftID, startTime, endTime, modifiedDate, numberOfHour, shiftDaysSchedule " +
+                "FROM Shift " +
+                "WHERE LOWER(shiftID) LIKE ?";
+        try (
+                Connection connection = DBHelper.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            preparedStatement.setString(1, "%" + input.toLowerCase() + "%");
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                Shift shift = new Shift();
+
+                shift.setStartTime(ConvertHelper.localTimeConverter(rs.getTime(2)));
+                shift.setNumberOfHour(rs.getInt(5));
+                shift.calcEndTime();
+
+                shift.setShiftID(rs.getString(1));
+                shift.setUpdatedDate(ConvertHelper.localDateTimeConverter(rs.getTimestamp(4)));
+                shift.setShiftDaysSchedule(ConvertHelper.shiftDaysScheduleConverter(rs.getString(6)));
+
+                data.add(shift);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
     public static void createData(Shift shift) {
         try (
                 Connection connection = DBHelper.getConnection();
@@ -142,6 +175,72 @@ public class ShiftDAO {
             exception.printStackTrace();
             System.exit(1);
         }
+    }
+
+    public static void updateDataWithTwoTableAtTheSameTime(String oldID, Shift shift) throws SQLException {
+        try (Connection conn = DBHelper.getConnection()) {
+            String updateTable1 = "UPDATE Shift SET shiftID = ? WHERE shiftID = ?";
+            String updateTable2 = "UPDATE ShiftAssignment SET shiftID = ? WHERE shiftID = ?";
+
+            try {
+                // Tắt chế độ tự động xác nhận
+                conn.setAutoCommit(false);
+
+                try (
+                        PreparedStatement stmt1 = conn.prepareStatement(updateTable1);
+                        PreparedStatement stmt2 = conn.prepareStatement(updateTable2);
+                ) {
+                    // Cập nhật shiftID trong Table1
+                    stmt1.setString(1, shift.getShiftID());
+                    stmt1.setString(2, oldID);
+                    stmt1.executeUpdate();
+
+                    // Cập nhật shiftID trong Table2
+                    stmt2.setString(1, shift.getShiftID());
+                    stmt2.setString(2, oldID);
+                    stmt2.executeUpdate();
+
+                    // Xác nhận giao dịch
+                    conn.commit();
+                    System.out.println("Cập nhật shiftID thành công trong cả hai bảng.");
+                } catch (SQLException e) {
+                    // Hủy giao dịch nếu có lỗi
+                    conn.rollback();
+                    System.err.println("Cập nhật thất bại. Đã hoàn nguyên thay đổi.");
+                    e.printStackTrace();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateDataWithOldID(String oldID, Shift shift) {
+        try (
+                Connection connection = DBHelper.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "UPDATE Shift " +
+                                "SET shiftID = ?, startTime = ?, endTime = ?, modifiedDate = ?, " +
+                                "numberOfHour = ?, shiftDaysSchedule = ? " +
+                                "WHERE shiftID = ? "
+                )
+        ){
+            preparedStatement.setString(1, shift.getShiftID());
+            preparedStatement.setTime(2, ConvertHelper.timeConvertertoSQL(shift.getStartTime()));
+            preparedStatement.setTime(3, ConvertHelper.timeConvertertoSQL(shift.getEndTime()));
+            preparedStatement.setTimestamp(4, ConvertHelper.dateTimeToSQLConverter(shift.getUpdatedDate()));
+            preparedStatement.setDouble(5, shift.getNumberOfHour());
+            preparedStatement.setString(6, ConvertHelper.shiftDaysScheduleConverterToSQL(shift.getShiftDaysSchedule()));
+            preparedStatement.setString(7, oldID);
+
+
+            preparedStatement.executeUpdate();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.exit(1);
+        }
 
     }
 
@@ -175,5 +274,60 @@ public class ShiftDAO {
         }
 
         return newShiftID;
+    }
+
+
+    public static boolean checkAllowUpdateAndDelete(String shiftID) {
+        ArrayList<Shift> data = new ArrayList<Shift>();
+        try (
+                Connection connection = DBHelper.getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT a.shiftID, b.employeeID " +
+                        "FROM Shift a " +
+                        "INNER JOIN ShiftAssignment b ON a.shiftID = b.shiftID " +
+                        "WHERE a.shiftID = ?");
+        ){
+            statement.setString(1,shiftID);
+            ResultSet rs = statement.executeQuery();
+
+            int rowCount = 0;
+            while (rs.next()) {
+                rowCount++;
+            }
+            if (rowCount >= 1) {
+                return false;
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.exit(1);
+        }
+
+        return true;
+    }
+
+    public static List<Employee> getEmployeeListByShift(String shiftID) {
+        List<Employee> data = new ArrayList<Employee>();
+        try (
+                Connection connection = DBHelper.getConnection();
+                PreparedStatement statement = connection.prepareStatement("SELECT a.shiftID, b.employeeID " +
+                        "FROM Shift a " +
+                        "INNER JOIN ShiftAssignment b ON a.shiftID = b.shiftID " +
+                        "WHERE a.shiftID = ?");
+        ){
+            statement.setString(1,shiftID);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                String employeeID = rs.getString(2);
+                Employee employee = EmployeeDAO.getDataByID(employeeID);
+                System.out.println(employee.toString());
+                data.add(employee);
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.exit(1);
+        }
+
+        return data;
     }
 }
