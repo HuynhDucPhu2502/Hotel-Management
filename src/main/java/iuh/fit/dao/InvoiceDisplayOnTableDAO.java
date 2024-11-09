3bc8c6cca4178f09cfa3e3760c34ffcbac92f79c
phatdang <phatdang19052004@gmail.com>
2024-11-09 07:06:18 +0700

    optimized

--- src/main/java/iuh/fit/dao/InvoiceDisplayOnTableDAO.java
+++ src/main/java/iuh/fit/dao/InvoiceDisplayOnTableDAO.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InvoiceDisplayOnTableDAO {
-    public static List<InvoiceDisplayOnTable> getData(){
+    public static List<InvoiceDisplayOnTable> getAllData(){
         List<InvoiceDisplayOnTable> data = new ArrayList<>();
         String SqlQuery = "select i.invoiceID, c.fullName, r.roomID, e.fullName, i.invoiceDate, rs.roomBookingDeposit, i.servicesCharge, i.roomCharge, t.taxRate, i.netDue\n" +
                 "from Invoice i join ReservationForm rs on i.reservationFormID = rs.reservationFormID\n" +
@@ -19,6 +19,48 @@ public class InvoiceDisplayOnTableDAO {
                 "join Employee e on e.employeeID = rs.employeeID\n" +
                 "join Room r on r.roomID = rs.roomID\n" +
                 "join Tax t on t.taxID = i.taxID";
+
+        try (
+                Connection connection = DBHelper.getConnection();
+                Statement statement = connection.createStatement();
+        ){
+            ResultSet rs = statement.executeQuery(SqlQuery);
+
+
+            while (rs.next()) {
+                InvoiceDisplayOnTable invoiceDisplayOnTable = new InvoiceDisplayOnTable();
+
+                invoiceDisplayOnTable.setInvoiceID(rs.getString(1));
+                invoiceDisplayOnTable.setCusName(rs.getString(2));
+                invoiceDisplayOnTable.setRoomID(rs.getString(3));
+                invoiceDisplayOnTable.setEmpName(rs.getString(4));
+                invoiceDisplayOnTable.setCreateDate(ConvertHelper.localDateTimeConverter(rs.getTimestamp(5)));
+                invoiceDisplayOnTable.setDeposit(rs.getDouble(6));
+                invoiceDisplayOnTable.setServiceCharge(rs.getDouble(7));
+                invoiceDisplayOnTable.setRoomCharge(rs.getDouble(8));
+                invoiceDisplayOnTable.setTax(rs.getDouble(9));
+                invoiceDisplayOnTable.setNetDue(rs.getDouble(10));
+
+                data.add(invoiceDisplayOnTable);
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            System.exit(1);
+        }
+
+        return data;
+    }
+
+    public static List<InvoiceDisplayOnTable> getDataThreeYearsLatest(){
+        List<InvoiceDisplayOnTable> data = new ArrayList<>();
+        String SqlQuery = "select i.invoiceID, c.fullName, r.roomID, e.fullName, i.invoiceDate, rs.roomBookingDeposit, i.servicesCharge, i.roomCharge, t.taxRate, i.netDue\n" +
+                "from Invoice i join ReservationForm rs on i.reservationFormID = rs.reservationFormID\n" +
+                "join Customer c on c.customerID = rs.customerID\n" +
+                "join Employee e on e.employeeID = rs.employeeID\n" +
+                "join Room r on r.roomID = rs.roomID\n" +
+                "join Tax t on t.taxID = i.taxID\n" +
+                "WHERE DATEPART(YEAR, i.invoiceDate) >= DATEPART(YEAR, GETDATE()) - 2 ";
 
         try (
                 Connection connection = DBHelper.getConnection();
