c1f41b374fcddcc850d523500cfbf31fdabebf40
phatdang <phatdang19052004@gmail.com>
2024-11-28 22:31:22 +0700

    test

--- src/main/java/iuh/fit/FetchTesting.java
+++ src/main/java/iuh/fit/FetchTesting.java
@@ -3,9 +3,13 @@ package iuh.fit;
 import iuh.fit.dao.RoomDisplayOnTableDAO;
 import iuh.fit.dao.ServiceDisplayOnTableDAO;
 import iuh.fit.models.enums.Month;
+import iuh.fit.security.PasswordHashing;
+import iuh.fit.utils.PropertiesFile;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Properties;
 
 public class FetchTesting {
     public static void main(String[] args) {
@@ -28,5 +32,12 @@ public class FetchTesting {
 //        String month = Month.valueOf(Arrays.stream(Month.values()).toList().get(0).toString()).getName();
 //        System.out.println(month);
         //System.out.println(RoomDisplayOnTableDAO.getData());
+        String key = PasswordHashing.hashPassword("admin");
+        String value = PasswordHashing.hashPassword("admin123");
+        String nameFile = PasswordHashing.hashPassword("password") + ".properties";
+
+        PropertiesFile.writeFile(nameFile, key, value, null);
+        System.out.println(PropertiesFile
+                .readFile(nameFile, key) + value);
     }
 }
